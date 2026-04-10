0a6591dc17777d9955fc2faca2fa2a65e8644476
Dogmatic Polack <kolowemployment@gmail.com>
2026-04-09 22:23:34 -0400

    Working on collision functions

--- src/SpacialGrid.zig
+++ src/SpacialGrid.zig
@@ -1,7 +1,18 @@
 const std = @import("std");
-const Ball = @import("./Ball.zig").Ball;
+pub const Vector2 = struct{
+    x: f32, 
+    y: f32,
 
-pub const Vector2 = struct{x: f32, y: f32};
+    pub fn eql(v1: Vector2, v2: Vector2) bool {
+        return (v1.x == v2.x and v1.y == v2.y);
+    }
+
+    pub fn getDistanceSq(v1: Vector2, v2: Vector2) f32 {
+        const dx: f32 = v1.x - v2.x;
+        const dy: f32 = v1.y - v2.y;
+        return (dx * dx + dy * dy);
+    }
+};
 
 pub const Config = struct {
     width: i32,
@@ -18,6 +29,7 @@ pub const CollisionData = struct {
 };
 
 pub const ShapeData = union{
+    Point: void,
     Rect: struct {w: f32, h: f32},
     Circle: f32,
 };
@@ -28,8 +40,8 @@ pub const SpacialGrid = struct {
         b: usize,
     };
 
-    width: i32,
-    height: i32,
+    width: f32,
+    height: f32,
     rows: usize, 
     cols: usize,
     cell_size: f32,
@@ -46,8 +58,8 @@ pub const SpacialGrid = struct {
         var self = SpacialGrid {
             .width = config.width,
             .height = config.height,
-            .rows = @intFromFloat(@ceil(@as(f32, @floatFromInt(config.height)) / config.cell_size)),
-            .cols = @intFromFloat(@ceil(@as(f32, @floatFromInt(config.width)) / config.cell_size)),
+            .rows = @ceil(@as(f32, @floatFromInt(config.height)) / config.cell_size),
+            .cols = @ceil(@as(f32, @floatFromInt(config.width)) / config.cell_size),
             .cell_size = config.cell_size,
             .ent_count = config.ent_count,
             .allocator = config.allocator,
@@ -220,15 +232,88 @@ pub const SpacialGrid = struct {
         return try self.all_collisions.toOwnedSlice(self.allocator);
     }
 
-    pub fn colliding(pos_a: Vector2, r_a: f32, pos_b: Vector2, r_b: f32) bool {
-        const dist = pos_a.distance(pos_b);
-        return (dist <= (r_a + r_b));
+    fn findNeighbors(
+        grid: *SpacialGrid, 
+        indecies: []usize, 
+        positions: []Vector2, 
+        shape_data: []ShapeData, 
+        col_list: *std.ArrayList(CollisionPair), 
+        query_buf: []usize
+    ) void {
+        for(indecies) |id_a| {
+            const pos_a = positions[id_a];
+            const shape_a = shape_data[id_a];
+
+            const nearby = grid.query(pos_a, query_buf) catch continue;
+
+            for(nearby) |id_b| {
+                if(id_a >= id_b) continue;
+
+                const pos_b = positions[id_b];
+                const shape_b = shape_data[id_b];
+
+                if(SpacialGrid.colliding(pos_a, shape_a, pos_b, shape_b)) {
+                    col_list.append(grid.allocator, .{ .a = id_a, .b = id_b }) catch continue;
+                }
+            }
+        }
     }
 
-    pub fn collidingRectCircle(rect_pos: Vector2, rect_dim: Vector2, circle_pos: Vector2, r: f32) bool {
-        const rect = raylib.Rectangle.init(rect_pos.x - rect_dim.x / 2, rect_pos.y - rect_dim.y / 2, rect_dim.x, rect_dim.y);
-        return raylib.checkCollisionCircleRec(circle_pos, r, rect);
+    pub fn checkColliding(pos_a: Vector2, shape_a: ShapeData, pos_b: Vector2, shape_b: ShapeData) bool {
+        switch (shape_a) {
+            .Circle => |r1| switch(shape_b) {
+                .Circle => |r2| circleCollision(pos_a, r1, pos_b, r2), 
+                .Rect => |dim| rectCircleCollision(pos_b, dim, pos_a, r1), 
+                .Point => pointCircleCollision(pos_a, r1, pos_b),
+            },
+            .Rect => |dim1| switch(shape_b) {
+                .Circle => |r| circleCollision(pos_b, r, pos_a, dim1), 
+                .Rect => |dim2| rectCollision(pos_a, dim1, pos_b, dim2), 
+                .Point => pointRectCollision(pos_a, dim1, pos_b)
+            },
+            .Point => switch(shape_b) {
+                .Circle => |r| pointCircleCollision(pos_b, r, pos_a),
+                .Rect => |dim| pointRectCollision(pos_b, dim, pos_a),
+                .Point => pointCollision(pos_b, pos_a),
+            }
+        }
     }
+
+    /// Get distance between two circles.  
+    pub fn circleCollision(pos_a: Vector2, r_a: f32, pos_b: Vector2, r_b: f32) bool {
+        const dist = Vector2.getDistanceSq(pos_a, pos_b);
+        const r = r_a + r_b;
+
+        return dist < (r * r);
+    }
+
+    /// Get distance between two Rectangles.  Assumes coordinates start at top left of rect.
+    pub fn rectCollision(pos_a: Vector2, dim_a: Vector2, pos_b: Vector2, dim_b: Vector2) bool {
+        return (
+            (pos_a.x < pos_b.x + dim_b.x and pos_a.x + dim_a.x > pos_b.x) 
+                                         and 
+            (pos_a.y < pos_b.y + dim_b.y and pos_a.y + dim_a.y > pos_b.y)
+        );
+    }
+
+    pub fn pointCollision(point1: Vector2, point2: Vector2) bool {
+        return Vector2.eql(point1, point2);
+    }
+
+    pub fn rectCircleCollision(rect_pos: Vector2, rect_dim: Vector2, circle_pos: Vector2, r: f32) bool {
+        _ = rect_pos; _ = rect_dim; _ = circle_pos; _ = r;
+    }
+
+    pub fn pointCircleCollision(pos_a: Vector2, r: f32, point: Vector2) bool {
+        const dist = Vector2.getDistanceSq(point, pos_a);
+        return (dist < r * r);
+    }
+
+
+    pub fn pointRectCollision(pos_a: Vector2, dim: Vector2, point: Vector2) bool {
+        _ = pos_a; _ = r; _ = point;
+    }
+
 };
 
 
